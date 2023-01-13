9d026a6a4c9492b80e0ae5b5f9498ef3d804627c
hotmanww <hotmanww@gmail.com>
2023-01-13 10:38:18 +0900

    TOCのリファクタリングとデザイン修正

---
+++ src/components/post-toc.tsx
@@ -0,0 +1,36 @@
+import TableOfContent from '../interfaces/tableOfContent'
+
+type Props = {
+  tableOfContent: TableOfContent[]
+}
+
+const PostTOC = ({ tableOfContent }: Props) => {
+  return (
+    <div className="hidden md:block w-72 ml-3">
+      <div className="flex flex-col sticky top-6">
+        <div className="p-4 shadow-md rounded-xl mb-6 bg-accent-1 ">
+          <p className="text-xl text-bold mb-4">目次</p>
+          <ul >
+            {tableOfContent.map((anchor: TableOfContent) => {
+              if (anchor.level === "H1") {
+                return (
+                  <li key={anchor.href}>
+                    <a href={anchor.href}>{anchor.title}</a>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={anchor.href}>
+                    <a href={anchor.href}>{anchor.title}</a>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostTOC
