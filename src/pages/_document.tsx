bb69569a0afe81e322ac562a89e54262aa36357b
hotmanww <hotmanww@gmail.com>
2023-01-30 19:12:41 +0900

    権限設定の変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
+        <Script src="https://embed.zenn.studio/js/listen-embed-event.js"></Script>
       </Head>
       <body className="bg-accent-2">
         <Main />
