4b1843a84b4f22c63fac5f5d3ecfeec6137a69a9
hotmanww <hotmanww@gmail.com>
2023-01-30 21:45:27 +0900

    エラー修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <Script src="https://embed.zenn.studio/js/listen-embed-event.js"></Script>
       </Head>
+      <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
       <body className="bg-accent-2">
         <Main />
         <NextScript />
