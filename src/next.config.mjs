82b64edf63e1148ef1b010f8fe5001f784380a6a
hotmanww <hotmanww@gmail.com>
2023-01-30 21:24:58 +0900

    fix bug

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,5 +1,12 @@
 import bundle_analyzer from "@next/bundle-analyzer";
+
 const withBundleAnalyzer = bundle_analyzer({
   enabled: process.env.ANALYZE === "true"
 });
-module.exports = withBundleAnalyzer({});
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = withBundleAnalyzer({})
+
+export default nextConfig
