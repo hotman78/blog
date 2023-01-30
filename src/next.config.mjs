5145900450600e624b0d16642f390098efdd8ca2
hotmanww <hotmanww@gmail.com>
2023-01-30 22:00:08 +0900

    script を内部に入れる

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -7,6 +7,6 @@ const withBundleAnalyzer = bundle_analyzer({
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = withBundleAnalyzer({})
+const nextConfig = withBundleAnalyzer({});
 
-export default nextConfig
+export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
       </Head>
-      <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
       <body className="bg-accent-2">
         <Main />
         <NextScript />
